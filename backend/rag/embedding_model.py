684d314397a482621e7ed1ecd73f171e9c074e27
RSV-exp <rapellysaivinil@gmail.com>
2026-05-30 23:19:14 +0530

    Adding the embedding model for re-using

---
+++ backend/rag/embedding_model.py
@@ -0,0 +1,12 @@
+#Instead of loading the embedding model again and again, we are just loading it once through this file and reusing it.
+
+from sentence_transformers import SentenceTransformer
+_embedding_model = None
+def get_embedding_model() -> SentenceTransformer:
+    global _embedding_model
+    if _embedding_model is None:
+        _embedding_model = SentenceTransformer(
+            "BAAI/bge-small-en-v1.5"
+        )
+        print("[INFO] Embedding model loaded successfully.")
+    return _embedding_model
